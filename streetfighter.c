78e99b2fcff0a0f6c07d78e2d7d887882bae7d44
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-02 22:50:17 +0000

    Oct 2, 2024, 10:50 PM

--- streetfighter.c
+++ streetfighter.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 
-    int ryu,ken;
+    int ryu,ken, RyuHP, KenHP;
 
     int main(void){
 
         printf("Valor do Golpe do Ryu: (numero positivo)\n");
+        scanf("%d", &ryu);
+        printf("Valor do Golpe do ken: (numero negativo)\n");
+        scanf("%d", &ken);
 
+        
     }
