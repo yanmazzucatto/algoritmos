0090e098cc80a5eec10dc938770ac88d81e8d93f
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-03 12:31:47 +0000

    Oct 3, 2024, 12:31 PM

--- streetfighter.c
+++ streetfighter.c
@@ -6,17 +6,21 @@
     int main(void){
 
         printf("Valor do Golpe do Ryu: (numero positivo)\n");
+        
+        //loop para contar o numero de rodadas jogadas
         for (int round =0; round < 3; round++ ){
 
         printf("Valor do Golpe do Ryu: (numero positivo)\n");
         scanf("%d", &ryu);
         if(ryu = ryu )
+        //tentativa de fazer for para numero perfeito
+
         RyuHP = RyuHP - ryu;
         printf("Valor do Golpe do ken: (numero negativo)\n");
         scanf("%d", &ken);
         KenHP = KenHP - ken;
         }
-
+        //verificação de quem ganhou 
         if (RyuHP > KenHP)
         {
             printf("ryu ganhou");
@@ -27,3 +31,5 @@
         }
         
 }
+
+(ryu *(ryu+1)/2)
