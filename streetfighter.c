8f64edd66dfd8d00c25d6268f9fd3aa442155c40
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-03 12:55:00 +0000

    Oct 3, 2024, 12:55 PM

--- streetfighter.c
+++ streetfighter.c
@@ -13,11 +13,12 @@
         printf("Valor do Golpe do Ryu: (numero positivo)\n");
         scanf("%d", &ryu);
         RyuHP = RyuHP - ryu;
+        numPerfeito(ryu);
 
         printf("Valor do Golpe do ken: (numero negativo)\n");
         scanf("%d", &ken);
         KenHP = KenHP - ken;
-        
+        numPerfeito(ken);
         }
         //verificação de quem ganhou 
         if (RyuHP > KenHP)
@@ -36,13 +37,13 @@
 
 
 // Função para verificar se um número é perfeito
-int numPerfeito(int ryu) {
+int numPerfeito(int combo) {
     int soma = 0; // Começa a soma dos divisores em 0
     // Verifica todos os números de 1 até n/2
-    for (int i = 1; i <= ryu/ 2; i++) {
-        if (ryu % i == 0) { // Se i é um divisor de n
+    for (int i = 1; i <= combo/ 2; i++) {
+        if (combo % i == 0) { // Se i é um divisor de n
             soma += i; // Adiciona i à soma
         }
     }
-    return soma == ryu*3; // Retorna 1 se a soma for igual a n
+    return soma == combo*3; // Retorna 1 se a soma for igual a n
 }
