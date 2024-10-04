c41a6dbfb317d917e34c76cec2f10faa4c6087ce
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-04 13:52:59 +0000

    Oct 4, 2024, 1:52 PM

--- lista de exercicios/streetfighter.c
+++ lista de exercicios/streetfighter.c
@@ -59,3 +59,23 @@ int numPerfeito(int combo) {
     }
 
 }
+// Função para verificar se um número é perfeito
+int numTriangular(int combo) {
+    int soma = 0; // Começa a soma dos divisores em 0
+    // Verifica todos os números de 1 até n
+
+    for (int i = 1; i < combo; i++) {
+        // Aqui você só está verificando se o número é divisível pelo combo
+        if (combo % i == 0) { // Se i é um divisor de n
+            // Aqui você só está somando se ele for divisível
+            soma += i;
+        }
+    }
+    if(soma == combo){
+        return combo * 3;
+        printf("\nES SE GOLPE FOI UM COMBO!!!! \n");
+    } else{
+        return combo;
+    }
+
+}
