aa25781085bf694fd90a18e36a7fca10f5a49203
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-04 10:46:14 +0000

    Oct 4, 2024, 10:46 AM

--- streetfighter.c
+++ streetfighter.c
@@ -40,11 +40,10 @@
 
 // Função para verificar se um número é perfeito
 int numPerfeito(int combo) {
-    printf("ENtrou no combo\n");
     int soma = 0; // Começa a soma dos divisores em 0
     // Verifica todos os números de 1 até n/2
 
-    for (int i = 1; i == combo; i++) {
+    for (int i = 1; i < combo; i++) {
         printf("ENtrou no for\n");
         // Aqui você só está verificando se o número é divisível pelo combo
         if (combo % i == 0) { // Se i é um divisor de n
