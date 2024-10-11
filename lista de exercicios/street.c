d6d6fc394654c206cb71c0cfbfe54922b181d71f
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-11 13:33:02 +0000

    Oct 11, 2024, 1:33 PM

---
+++ lista de exercicios/street.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int ryu, ken, round, golpe = 1;
+
+    printf("Digite o valor dos golpes: \n");
+    scanf("%d", &golpe);
+
+    while (golpe != 0 )
+    {
+        
+    }
+    
+
+}
