dd691f24b83be88d24e8517fcf379befe73e9418
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-11 13:13:24 +0000

    Oct 11, 2024, 1:13 PM

---
+++ aula7/fatoracao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Digite um número Inteiro: (nao muito grande em!)\n");
+    scanf("%d" , &num);
+
+    for ( int i = 0; i < num ; i++ ){
+        
+    }
+    
+}
