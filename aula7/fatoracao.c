e6a896496813a953e8fdc67551793936d793e367
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-11 13:16:04 +0000

    Oct 11, 2024, 1:16 PM

--- aula7/fatoracao.c
+++ aula7/fatoracao.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int num;
+    int num, aux, somatoria;
 
     printf("Digite um número Inteiro: (nao muito grande em!)\n");
     scanf("%d" , &num);
 
-    for ( int i = 0; i < num ; i++ ){
+    aux = num;
+    
+    for ( int i = 0; i < aux ; i++ ){
         
     }
     
