1cdd84033b67b73623987ce593074310b2e35802
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-25 10:57:27 +0000

    Oct 25, 2024, 10:57 AM

--- matriz/teste1.c
+++ matriz/teste1.c
@@ -6,10 +6,16 @@ int main(){
     
     for (int i = 0; i < 5; i++){
         for( int j = 0; j < 2; j ++ ){
-        printf("digite a nota %d", j + 1);
+        printf("digite a nota %d:  ", j + 1);
         scanf("%d", &matriz[i][j]);
         }
     }
+    for ( i = 0; i < 5; i++);{
+        int soma = 0;
+    } for(j = 0; j <2; j++);
+        soma += matriz[i][j];
     
+
+    int media[5] = (i + j) /2;
 
 }
