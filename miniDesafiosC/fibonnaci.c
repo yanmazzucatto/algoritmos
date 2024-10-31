ab0c87c0b7a8d0fd288c1d5790ecbac8d8d9d792
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-31 13:38:32 +0000

    Oct 31, 2024, 1:38 PM

--- miniDesafiosC/fibonnaci.c
+++ miniDesafiosC/fibonnaci.c
@@ -8,9 +8,11 @@ int main(){
     scanf("%d", &n1);
     
     n2 = n1;
-    for ( int i = 0; i <= 100; i++)
+    for ( int i = 0; i <= 10; i++)
     {
-        n1 = n1 + n1;
+        n1 += n2;
+        n2 = n1;
+
         printf("\n valores - fibonnaci: %d", n1 );
     }
     //interminado
