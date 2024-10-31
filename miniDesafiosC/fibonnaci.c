35c05c6a4d6c732f8ac5ccaba1dc453101d34743
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-10-31 13:18:27 +0000

    Oct 31, 2024, 1:18 PM

---
+++ miniDesafiosC/fibonnaci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int n1, n2;
+
+    printf("digite o primeiro numero");
+    scanf("%d", &n1);
+    
+    n2 = n1;
+    for ( int i = 0; i <= 100; i++)
+    {
+        
+    }
+    
+
+}
