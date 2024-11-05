71a73158ef85001d5304434a6e908d35e574de20
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-11-05 13:49:30 +0000

    Nov 5, 2024, 1:49 PM

--- aula8/megasena.c
+++ aula8/megasena.c
@@ -7,14 +7,11 @@ int main() {
     printf("Digite o valor de n: ");
     scanf("%d", &n);
     
-    // Laço externo: para cada linha de 1 até n
     for (int i = 1; i <= n; i++) {
-        // Laço interno: para imprimir os números de 1 até o valor da linha
         for (int j = 1; j <= i; j++) {
-            printf("%d ", j);  // Imprime o número j seguido de um espaço
+            printf("%d ", j);  
         }
         
-        // Depois de imprimir todos os números na linha, quebra a linha
         printf("\n");
     }
     
