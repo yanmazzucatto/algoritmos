17ffab4b2d1c46badafc9665b171e51ac8f98d14
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-11-12 13:47:23 +0000

    Nov 12, 2024, 1:47 PM

--- aulasVetores/vetores1.c
+++ aulasVetores/vetores1.c
@@ -4,5 +4,12 @@ int main(){
     int  vet1[5];
     int vet2[5];
 
-    
+    for( int i = 0; i >= 5; i++ ){
+
+        for ( int j = 0; j >= 5 ; j++)
+        {
+            
+        }
+        
+    }
 }
