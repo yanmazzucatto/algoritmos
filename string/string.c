23ffb09723665dcf4d9e9dccdfbc64f877dd7939
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-11-22 13:54:55 +0000

    Nov 22, 2024, 1:54 PM

--- string/string.c
+++ string/string.c
@@ -3,8 +3,8 @@
 int main (){
     char string[79];
 
-    for(int i = 0; i <= 79; i++ ){
     printf("Digite 79 caracteres: \n");
-    scanf("%s", &string[i]);
+    for(int i = 0; i <= 79; i++ ){
+        scanf("%s", &string[i]);
     }
 }
