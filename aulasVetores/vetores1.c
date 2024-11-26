6893b4a76ca02bf38141faac9552c253c777dd7e
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2024-11-26 13:11:38 +0000

    Nov 26, 2024, 1:11 PM

--- aulasVetores/vetores1.c
+++ aulasVetores/vetores1.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-int main(){
-    int  vet1[5];
-    int vet2[5];
+int main() {
+    
+    int numeros[100];
 
-    for( int i = 0; i >= 5; i++ ){
-
-        for ( int j = 0; j >= 5 ; j++)
-        {
-            
-        }
-        
+    //preenchendo o vetor com os números de 1 a 100.
+    for (int i = 0; i < 100; i++) {
+        numeros[i] = i + 1; // Preenche o vetor com números de 1 a 100
     }
+
+    //imprimir os números em ordem crescente.
+    printf("Ordem crescente:\n");
+    for (int i = 0; i < 100; i++) {
+        printf("%d ", numeros[i]);
+    }
+
+    //imprimir os números em ordem decrescente.
+    printf("\nOrdem decrescente:\n");
+    for (int i = 99; i >= 0; i--) {
+        printf("%d ", numeros[i]);
+    }
+
+    return 0;
 }
