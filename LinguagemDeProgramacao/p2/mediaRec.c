08a89ed14263194e4a25e1ce2011d8ae3f3bbb35
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2025-07-09 11:58:01 +0000

    recursividade para média pronta

--- LinguagemDeProgramacao/p2/mediaRec.c
+++ LinguagemDeProgramacao/p2/mediaRec.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Função recursiva que retorna o menor valor no vetor
-int min_rec(int vet[], int index, int tamanho) {
-    // Caso base: se for o último elemento, retorne ele
-    if (index == tamanho - 1) {
-        return vet[index];
+
+int media(int *v, int aux, int i, int j){
+    
+    printf("digite o valor da posicao %d:  ", j);
+    scanf("%d", &v[j]);
+    aux = v[j] + aux;
+    j++;
+    if (j < i)
+    {
+    aux = media( v , aux , i , j );    
     }
+    else{
+        return aux/i;
+    }
+    
 
-    // Chamada recursiva para o próximo índice
-    int min_do_resto = min_rec(vet, index + 1, tamanho);
-
-    // Retorna o menor entre o atual e o resto
-    return (vet[index] < min_do_resto) ? vet[index] : min_do_resto;
 }
 
-int main() {
-    int vet[] = {4, 5, 7, 11, 1, 23, 15};
-    int tamanho = sizeof(vet) / sizeof(vet[0]);
 
-    // Chamada da função recursiva a partir do índice 0
-    int menor = min_rec(vet, 0, tamanho);
+int main(){
 
-    // Impressão do resultado
-    printf("menor valor do vetor: %d\n", menor);
+    int *v;
+    int aux = 0, i = 3, j = 0;
+        printf("digite o numero de posicoes do vetor: ");
+        scanf("%d", &i);
+    v = malloc(i * sizeof(double));
+    aux = media( v, aux, i, j);
+    printf("A média arrendodada é: %d \n ", aux);
 
-    return 0;
 }
