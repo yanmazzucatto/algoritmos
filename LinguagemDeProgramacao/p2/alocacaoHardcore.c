9df28ea98d6a26149bfee8e35ed4eb7a159799e4
Yan Mazzucatto <113943980+yanmazzucatto@users.noreply.github.com>
2025-07-10 01:23:07 +0000

    tentando fazer alocacao hardcoreee

---
+++ LinguagemDeProgramacao/p2/alocacaoHardcore.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tamanhoVet(int n){
+    printf("quantas posicoes voce quer? \n");
+    scanf("%d", &n);
+    return n;
+}
+
+int* preencher(int n ){
+
+    int *v = malloc(n*(sizeof(int)));
+    for (int i = 0; i < n; i++)
+    {
+        printf("digite o numero dessa posicao:  ");
+        scanf("%d", &v[i]);
+    }
+
+    return v;
+}
+
+void encontrarMaiores(int *v, int n){
+
+    for (int i = 0; i < n; i++)
+    {
+        if(*v[i]>*v[i++]){
+            encontrarMaiores(*v, n);
+        }else{
+            *v[i] = *v[i++];
+            printf("%d", *v[i], "\0\0\0 %d", *v[i++]);
+        }
+    }
+    
+}
+
+
+int main(){
+    int n, *v;
+    n = tamanhoVet(n);
+    v = preencher( *v,  n );
+    *v = encontrarMaiores( *v,  n);
+}
--- LinguagemDeProgramacao/p2/fibonacciRec.c
+++ LinguagemDeProgramacao/p2/fibonacciRec.c
@@ -2,19 +2,20 @@
 #include <stdlib.h>
 
 int fibonacci( int n ){
+if (n == 0) return 0;
+if (n == 1) return 1;
 
-if (n + n == 0){
-    fibonacci(n++);
-}else{
-
-
-}
-
+if (n >= 100) {
+    printf("%d", n);
+    break;
+} else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 int main(){
 
-int n = 0, aux = 0, aux2 = 0;
+int n = 0,
 n = fibonacci(n);
 
 
